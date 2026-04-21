4cbee48230fd8ddf3dc7e8b4380da56d02964aec
Stephen Nyamali <stephen.nyamali@gmail.com>
2026-04-21 16:17:08 +0100

    feat: improve DebouncedButton logic to handle submit button state and disable conditions

--- client/src/components/common/DebouncedButton.tsx
+++ client/src/components/common/DebouncedButton.tsx
@@ -7,15 +7,21 @@ type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 const DebouncedButton = React.forwardRef<HTMLButtonElement, Props>(
   ({ debounceMs = 800, onClick, disabled, children, ...rest }, ref) => {
     const [isDisabled, setIsDisabled] = React.useState(false);
+    const isSubmitButton = rest.type === "submit";
 
     const handleClick: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-      if (isDisabled || disabled) return;
+      if ((!isSubmitButton && isDisabled) || disabled) {
+        e.preventDefault();
+        return;
+      }
       try {
         // eslint-disable-next-line @typescript-eslint/no-unused-expressions, @typescript-eslint/no-explicit-any
         onClick && onClick(e as any);
       } finally {
-        setIsDisabled(true);
-        setTimeout(() => setIsDisabled(false), debounceMs);
+        if (!isSubmitButton) {
+          setIsDisabled(true);
+          setTimeout(() => setIsDisabled(false), debounceMs);
+        }
       }
     };
 
@@ -24,7 +30,7 @@ const DebouncedButton = React.forwardRef<HTMLButtonElement, Props>(
         {...rest}
         ref={ref}
         onClick={handleClick}
-        disabled={isDisabled || disabled}
+        disabled={(!isSubmitButton && isDisabled) || disabled}
       >
         {children}
       </button>
